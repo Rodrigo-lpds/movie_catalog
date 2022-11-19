de62619c78cc9f452f69f64ccaf6e93ade9f7155
Lucemila Bonifacio <103540532+LucemilaBonifacio@users.noreply.github.com>
2022-11-19 20:11:17 -0300

    add comment scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_19_223243) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_19_224910) do
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.boolean "status"
+    t.integer "movie_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_comments_on_movie_id"
+  end
+
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "release_year"
@@ -18,4 +27,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_19_223243) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "movies"
 end
