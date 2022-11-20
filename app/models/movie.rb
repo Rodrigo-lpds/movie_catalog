ce800c9bd50007dfd7fc207af1da6ec7dc34dc41
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-11-20 11:44:51 -0300

    add comment relation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
     validates :title, :release_year, presence: true
     validates :title, uniqueness: true
-    validates :release_year, comparison: { less_than:2022 }
+    # validates :release_year, comparison: { less_than:2022 }
     has_many :comments
 
 end
