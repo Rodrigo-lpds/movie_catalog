1ad56fe860eac07691240accd8ebe3a21d6d3796
Lucemila Bonifacio <103540532+LucemilaBonifacio@users.noreply.github.com>
2022-11-20 09:46:06 -0300

    Validates log and movies

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,3 +1,4 @@
 class Log < ApplicationRecord
+  validates :approved_date, presence: true
   belongs_to :comment
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
+    validates :title, :release_year, presence: true
+    validates :title, uniqueness: true
+    validates :release_year, comparison: { less_than:2022 }
     has_many :comments
+
 end
