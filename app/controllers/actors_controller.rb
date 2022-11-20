7053cc8f91b3943a10f000fe951399661f8cbeb4
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-11-20 10:27:45 -0300

    update crud organization of actor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,70 +1,55 @@
 class ActorsController < ApplicationController
-  before_action :set_actor, only: %i[ show edit update destroy ]
+  before_action :set_actor, only: %i[show edit update destroy]
 
-  # GET /actors or /actors.json
   def index
     @actors = Actor.all
   end
 
-  # GET /actors/1 or /actors/1.json
-  def show
-  end
+  def show; end
 
-  # GET /actors/new
   def new
     @actor = Actor.new
   end
 
-  # GET /actors/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /actors or /actors.json
   def create
     @actor = Actor.new(actor_params)
 
     respond_to do |format|
       if @actor.save
-        format.html { redirect_to actor_url(@actor), notice: "Actor was successfully created." }
-        format.json { render :show, status: :created, location: @actor }
+        format.html { redirect_to actor_url(@actor), notice: 'Actor was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @actor.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /actors/1 or /actors/1.json
   def update
     respond_to do |format|
       if @actor.update(actor_params)
-        format.html { redirect_to actor_url(@actor), notice: "Actor was successfully updated." }
-        format.json { render :show, status: :ok, location: @actor }
+        format.html { redirect_to actor_url(@actor), notice: 'Actor was successfully updated.' }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @actor.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /actors/1 or /actors/1.json
   def destroy
     @actor.destroy
 
     respond_to do |format|
-      format.html { redirect_to actors_url, notice: "Actor was successfully destroyed." }
-      format.json { head :no_content }
+      format.html { redirect_to actors_url, notice: 'Actor was successfully destroyed.' }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_actor
-      @actor = Actor.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def actor_params
-      params.require(:actor).permit(:name, :birth_date, :email)
-    end
+  def set_actor
+    @actor = Actor.find(params[:id])
+  end
+
+  def actor_params
+    params.require(:actor).permit(:name, :birth_date, :email)
+  end
 end
--- app/views/actors/_actor.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! actor, :id, :name, :birth_date, :email, :created_at, :updated_at
-json.url actor_url(actor, format: :json)
